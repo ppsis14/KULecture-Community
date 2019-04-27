6756b807bf536ea08b26d535c3968ab6252305bb
pk <keattisak.t@ku.th>
2019-04-27 23:53:15 +0700

    auth admin, admin can login to dashboard

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,8 @@
 namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Auth;
+
 class LoginController extends Controller
 {
     /*
@@ -20,7 +22,8 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = 'admin/dashboard';
+
     /**
      * Create a new controller instance.
      *
@@ -30,4 +33,19 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    public function showLoginForm()
+    {
+        return view('layouts.admin.admin-login');
+    }
+
+    protected function guard() {
+        return Auth::guard('admin');
+    }
+
+    public function username()
+    {
+        return 'username';
+    }
+
 }
