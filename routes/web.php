fd0ead1b9fa1f00480e11e50357365cc8260b90a
pannakarn <pannakarn.p@ku.th>
2019-05-06 07:09:50 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,9 @@ Route::get('/', function () {return view('welcome');});
 Route::get('/admin/login', 'AdminsLoginController@index');
 // Route::post('/admin/login', 'AdminsLoginController@login');
 Route::post('/admin/insert','AddNewAdminController@store');
+
 Route::get('/', function () {return view('index');});
+
 Route::get('/admin/login', 'AdminsLoginController@index');
 Route::get('/admin/dashboard', 'AdminDashBoardController@showDashBoard');
 Route::get('/admin/users', 'UsersManagementController@index');
@@ -23,21 +25,17 @@ Route::delete('/admin/users/{id}', 'UsersManagementController@destroy');
 Route::get('/admin/posts', 'PostsManagementController@index');
 Route::get('/admin/addadmin', 'AddNewAdminController@index');
 Route::get('/admin/changepassword', 'ChangePasswordController@index');
-<<<<<<< HEAD
-Route::post('/admin/changepw','ChangePasswordController@update');
-=======
 
 Route::post('/admin/changepassword','ChangePasswordController@update');
 Route::get('/admin/notifications', 'AdminNotificationsController@index');
 Route::post('/admin/changepw','ChangePasswordController@update');
 
->>>>>>> pair_dev
 Route::get('/user/login', 'UsersLoginController@index');
-
 Route::get('/user/explorer', 'ExplorePostsController@index');
 Route::get('/user/explorer/search/{category}', 'ExplorePostsController@search');
 Route::get('/user/explorer/advance', 'ExplorePostsController@advance');
 Route::get('/user/explorer/category/{category}', 'ExplorePostsController@category');
+
 Route::get('/user/explorer/tag/{tag}', 'TagsController@show');
 Route::get('/user/explorer/tag', 'TagsController@advance');
 
@@ -55,5 +53,6 @@ Route::get('login/google', 'Auth\LoginGoogleController@redirectToProvider');
 Route::get('login/google/callback', 'Auth\LoginGoogleController@handleProviderCallback');
 Auth::routes();
 // Route::get('/home', 'HomeController@index')->name('home');
+
 //for notification
 Route::get('notification','AddNewAdminController@notification');
